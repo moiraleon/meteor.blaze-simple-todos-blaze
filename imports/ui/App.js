21ee2a7a85cd0ba1f2f6d8165c306a0c73dd207d
Moira Leon <moira.leon2@gmail.com>
2021-11-15 15:44:57 -0700

    3.5: Show Newest Tasks First

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,9 +1,29 @@
 import { Template } from 'meteor/templating';
-import { TasksCollection } from "../api/TasksCollection";
+import { TasksCollection } from '../api/TasksCollection';
 import './App.html';
 
 Template.mainContainer.helpers({
   tasks() {
-    return TasksCollection.find({});
+    return TasksCollection.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.form.events({
+  'submit .task-form'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const { target } = event;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    TasksCollection.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
   },
 });
