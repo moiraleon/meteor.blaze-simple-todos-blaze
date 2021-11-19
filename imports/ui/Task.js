210a2b337b57ccddc24bfe7893c0c8d1f615e387
Moira Leon <moira.leon2@gmail.com>
2021-11-19 15:41:53 -0700

    styling update +pair programming

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -4,11 +4,16 @@ import { Template } from 'meteor/templating';
 import './Task.html';
 
 Template.task.events({
-  'click .toggle-checked'() {
+  'click .toggle-checked'(e) {
     // Set the checked property to the opposite of its current value
     Meteor.call('tasks.setIsChecked', this._id, !this.isChecked);
+    if(!this.isChecked){
+      const selectedItem = e.target.closest(".taskBox");
+     selectedItem.style.background = 'green';
+     }
   },
   'click .delete'() {
     Meteor.call('tasks.remove', this._id);
   },
 });
+
